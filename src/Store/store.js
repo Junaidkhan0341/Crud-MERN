b1884a764068a3e238c2ab31db6a6d293593bc66
Junaidkhan0341 <junaidkh0092@gmail.com>
2023-12-24 21:19:10 +0500

    Updates

--- src/Store/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import itemReducer from "../Features/itemReducer";
-
-export const store = configureStore({
-  reducer: { 
-    app: itemReducer,
-  },
-});
